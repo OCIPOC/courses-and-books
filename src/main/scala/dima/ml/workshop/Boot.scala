9bb16f3d8bfb6271ad35b2897323749f3e342ec5
Dima Statz <dima.statz@cloudlock.com>
2018-06-17 13:49:30 +0300

    Initial commit

---
+++ src/main/scala/dima/ml/workshop/Boot.scala
@@ -0,0 +1,17 @@
+package dima.ml.workshop
+
+import org.apache.spark.sql.SparkSession
+
+object Boot {
+  def main(args: Array[String]): Unit = {
+    println("starting spark")
+
+    val session = SparkSession
+      .builder()
+      .appName("normalizer")
+      .master("local[*]")
+      .getOrCreate()
+
+    Titanic.explore(session)
+  }
+}
