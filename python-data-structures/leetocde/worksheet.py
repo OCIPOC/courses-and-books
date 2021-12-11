be12e6d136d5e77b455bde48261efae2ce182334
dimastatz <dima.statz@gmail.com>
2021-12-11 08:14:30 +0200

    add solution for longest unique substring

--- python-data-structures/leetocde/worksheet.py
+++ python-data-structures/leetocde/worksheet.py
@@ -1,11 +1,3 @@
-from typing import NamedTuple
-
-class Node(NamedTuple):
-    x: int
-    next: NamedTuple
-
-n = Node(1, None)
-print(n)
-
-n.next = Node(2, None)
-print(n)
+for i in range(0, 3):
+    print(i)
+    
