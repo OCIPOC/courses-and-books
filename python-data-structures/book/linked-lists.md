db66874bacc0c4c9abecdc644b06cd52a978efb7
dimastatz <dima.statz@gmail.com>
2021-12-16 21:35:10 +0200

    add linked list

--- python-data-structures/book/linked-lists.md
+++ python-data-structures/book/linked-lists.md
@@ -12,6 +12,39 @@ class Node:
         self.element: str = element
         self.next: Node = next
 ```
+In Python you can find a linked list implementationin in [collection.deque](https://github.com/python/cpython/blob/main/Modules/_collectionsmodule.c#L71). Deque can be used as stack and queue. Implementing stack and queue using linked lists is easy. For example, see stack implementation:
+```Python
+class Stack:
+    def __init__(self) -> None:
+        self.size = 0
+        self.head: Node = None
+        
+    def push(self, element):
+        node = Node(element, None)
+        if self.head:
+            node.next = self.head
+        self.head = node
+        self.size += 1
+        
+    def pop(self):
+        if self.size == 0:
+            raise Exception('stack is empty')
+        self.size -= 1
+        item = self.head
+        self.head = self.head.next
+        return item.element
+
+    def top(self):
+        if self.size == 0:
+            raise Exception('stack is empty')
+        return self.head.element
+
+    def len(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+```
 
 
 ## Circularly Linked Lists
--- python-data-structures/leetocde/worksheet.py
+++ python-data-structures/leetocde/worksheet.py
@@ -1,24 +1,59 @@
-from _typeshed import Self
-
-
 class Node:
     def __init__(self, element, next=None) -> None:
         self.element: str = element
         self.next: Node = next
 
-class LinkedList:
+class Stack:
     def __init__(self) -> None:
+        self.size = 0
         self.head: Node = None
         
-    def add_first(self, element):
+    def push(self, element):
         node = Node(element, None)
         
         if self.head:
-            node.next = self.head.next
+            node.next = self.head
+
         self.head = node
-
-        if not self.tail:
-            self.tail = node
+        self.size += 1
         
-    def remove()
+    def pop(self):
+        if self.size == 0:
+            raise Exception('stack is empty')
+        
+        self.size -= 1
+        item = self.head
+        self.head = self.head.next
+        
+        return item.element
 
+    def top(self):
+        if self.size == 0:
+            raise Exception('stack is empty')
+        return self.head.element
+
+    def len(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def __str__(self):
+        s = []
+        current = self.head
+        while current:
+            s.append(str(current.element))
+            current = current.next
+        return '->'.join(s)
+
+
+s = Stack()
+print(s)
+
+print(s.push(1), s)
+print(s.push(2), s)
+print(s.push(3), s)
+print(s.len(), s.pop(), s)
+print(s.len(), s.pop(), s)
+print(s.len(), s.pop(), s)
+print(s.len())
