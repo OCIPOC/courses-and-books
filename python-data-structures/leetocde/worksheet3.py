18352b3de6a551e3550b27df530fed7d97830f01
dimastatz <dima.statz@gmail.com>
2021-12-27 13:19:57 +0200

    add k-largest solution

--- python-data-structures/leetocde/worksheet3.py
+++ python-data-structures/leetocde/worksheet3.py
@@ -1,24 +1,7 @@
-import math
-
-class Node:
-    def __init__(self, right, left) -> None:
-        self.left: Node = left
-        self.right: Node = right
-        
-
-def build_tree(nodes: list) -> Node:
-    q = [0]
-    root = Node()
-    while q:
-        
+import heapq
 
 
+heap = [1, 2, 3]
+heapq.heapify(heap)
 
-
-def get_highest_score_nodes(nodes: list) -> int:
-    pass
-
-assert(get_highest_score_nodes([-1,2,0,2,0]) == 3)
-assert(get_highest_score_nodes([-1,2,0]) == 2)
-assert(get_highest_score_nodes([-1,0]) == 2)
-assert(get_highest_score_nodes([-1,3,3,5,7,6,0,0]) == 2)
+heapq.nlargest(2, heap)
