5994e743d73d595ab2b39af7960cd878c3355b62
dimastatz <dima.statz@gmail.com>
2021-12-29 21:56:53 +0200

    fix spelling

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,38 +1,20 @@
-class TreeNode:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-def from_list(tree: list, root: TreeNode=None):
-    if not tree:
-        return None
-    elif not root:
-        root = TreeNode(tree.pop(0))
-        return from_list(tree, root)
-    else:
-        root.left = TreeNode(tree.pop(0))
-        root.right = TreeNode(tree.pop(0))
-        from_list(tree, root.left)
-        from_list(tree, root.right)
-
-    return root
-
-
-def bf_to_list(node: TreeNode):
-    res, q = [], [node]
-    while q:
-        tmp: TreeNode = q.pop(0)
-        if tmp:
-            res.append(tmp.val)
-            q.append(tmp.left)
-            q.append(tmp.right)
-    return res
-
-
-root = from_list([5, 1, 4, None, None, 3, 6])
-print(bf_to_list(root))
-
-
-print([1, 2, 3].index(2))
+def three_sum(nums: list) -> list:
+    nums.sort()
+    ans = []
+        
+    for i in range(len(nums)):
+        j, k = i+1, len(nums)-1
+            
+        while j < k:
+            if nums[i] + nums[j] + nums[k]  > 0:
+                k -= 1                    
+            elif nums[i] + nums[j] + nums[k] < 0:
+                j += 1
+            else:
+                l = [nums[i],nums[j],nums[k]]
+                if l not in ans:
+                    ans.append(l)
+                j, k = j + 1, k - 1  
+    return ans
+        
+print(three_sum([-1,0,1,2,-1,-4]))
