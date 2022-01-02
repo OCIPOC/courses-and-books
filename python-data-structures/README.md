6c2affde08a3217b91505da57bd1d890cdae0cba
dimastatz <dima.statz@gmail.com>
2022-01-02 19:11:37 +0200

    add bitwise operations

--- python-data-structures/README.md
+++ python-data-structures/README.md
@@ -21,7 +21,7 @@ In addition, in the [leetcode](https://github.com/dimastatz/courses-and-books/tr
 - [Memory Management And B-Trees]()
 - [Appendix A: Character Strings in Python]()
 - [Appendix B: Useful Mathematical Facts]()
-- [Appendix C: Bitwise opeartion in Python]()  
+- [Appendix C: Bitwise opeartion in Python](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/book/bitwise-operations.md)  
 
 ## LeetCode Problems
 - [Two Sum](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/leetocde/two-sum.ipynb)
--- python-data-structures/leetocde/worksheet2.py
+++ python-data-structures/leetocde/worksheet2.py
@@ -1,29 +1,18 @@
-def fourSum(nums: list, target: int) -> list:
-    nums.sort()
+def subtract(x, y):
+    if y == 0:
+        return x
+    else:
+        return subtract(x ^ y, ((~x) & y) << 1)
 
-    def two_sum(nums: list, target: int, start) -> list:
-        res = []
-        stop = len(nums) - 1
-        while start < stop:
-            if nums[start] + nums[stop] == target:
-                res.append([nums[start], nums[stop]])
-                start, stop = start + 1, stop - 1
-            elif nums[start] + nums[stop] > target:
-                stop -= 1
-            else:
-                start += 1
-        return res
-        
-    res = []
-    for i in range(len(nums) - 3):
-        for j in range(i + 1, len(nums) - 2):
-            tmp_target = target - (nums[i] + nums[j])
-            if tmp_target < 0:
-                continue
-            else:
-                tmp_res = two_sum(nums, tmp_target, j + 1)     
-                res.append([[nums[i], nums[j]] + x for x in tmp_res if x])
+
+def divide(x, y):
+    res = 0
+    while x > 0:
+        x = subtract(x, y)
+        res += 1
     return res
 
+print(subtract(10, -2))
+print(divide(600, 15))
 
-fourSum([1,0,-1,0,-2,2], 0)
+
