b7844d672a57b4b19a8674537edfece3a93ed0f4
dimastatz <dima.statz@gmail.com>
2022-01-03 14:52:25 +0200

    fb screening

--- python-data-structures/leetocde/worksheet2.py
+++ python-data-structures/leetocde/worksheet2.py
@@ -1,23 +1,11 @@
-from time import sleep
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from typing import NamedTuple
 
 
-def build(nums: list, start, end, msg = '') -> TreeNode:
-    print(nums, start, end, msg)
-    sleep(1)
-    if start < end:
-        mid = (end - start) // 2
-        left = build(nums, start, mid, 'l')
-        right = build(nums, mid + 1, end, 'r')
-        return TreeNode(nums[mid], left, right)
-    else:
-        return None
+from typing import NamedTuple
+
+class A(NamedTuple):
+    a: str
 
 
-nums = [-10,-3,0,5,9]
-print(build(nums, 0, len(nums)-1))
+a = A('')
+print(a)
