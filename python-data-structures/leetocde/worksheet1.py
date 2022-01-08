22d12698bf2932072413a2f91e6365c7e68dbd5f
dimastatz <dima.statz@gmail.com>
2022-01-08 21:42:02 +0200

    add first problem from goog

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,17 +1,3 @@
-from functools import reduce
+s = {1}
 
-def merge(intervals: list) -> list:
-    intervals.sort()
-    start = 1
-    while start < len(intervals):
-        if intervals[start][0] <= intervals[start - 1][1]:
-            end = max(intervals[start][1], intervals[start - 1][1])
-            intervals[start - 1][1] = end
-            intervals.pop(start)
-        else:
-            start = start + 1
-    
-    return intervals
-
-assert merge([[1,4],[4,5]]) == [[1,5]]
-assert merge([[1,3],[2,6],[8,10],[15,18]]) == [[1,6],[8,10],[15,18]]
+print(list())
