c6b7d6b82fb1c94debbca67b9ee6e6af6e3ad05a
dimastatz <dima.statz@gmail.com>
2022-01-10 10:55:17 +0200

    change workspaces

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,4 +1,16 @@
-lst = [1]
+from typing import List
 
-mid = len(lst) // 2
-print(lst[0: mid], lst[mid:])
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    for i in range(m, len(nums1)):
+        print(i, m, i -m)
+        nums1[i] = nums2[i - m]
+
+    nums1.sort()
+    print(nums1)
+
+
+print(merge(nums1=[1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n=3))
