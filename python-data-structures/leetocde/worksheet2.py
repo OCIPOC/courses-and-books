fdf3084fd3f37e5da1bf9e8c4aa51e95620152c9
dimastatz <dima.statz@gmail.com>
2022-01-15 19:17:38 +0200

    lru-cache

--- python-data-structures/leetocde/worksheet2.py
+++ python-data-structures/leetocde/worksheet2.py
@@ -1,7 +1,13 @@
-from heapq import heapify
+from collections import OrderedDict
+
+cache = OrderedDict()
+cache[1] = 1
+
+print(cache)
+
+cache.move_to_end(1)
+print(cache)
 
 
-import heapq
-
-
-heapq.nsmallest(1, )
+cache.popitem()
+print(cache)
