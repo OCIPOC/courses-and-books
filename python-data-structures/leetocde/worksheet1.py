327fc2060c71e2a1d9a4d8ca0158673b00358e79
dimastatz <dima.statz@gmail.com>
2022-01-18 23:05:27 +0200

    add longest substring 2 chars

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,4 +1,16 @@
-from random import random
+def len_distinct(s: str) -> int:
+    chars = {}
+    max_len, start, current = 0, 0, 0
+    while current < len(s):
+        chars[s[current]] = current
+        if len(chars) > 2:
+            min_item = min([x for x in chars.items()], key=lambda x: x[1])
+            del chars[min_item[0]]
+            start = min_item[1] + 1
+        if max_len < (current - start) + 1:
+            max_len = (current-start) + 1
+        current +=1
+    return max_len
 
-
-random
+print(len_distinct("eceba"))
+# print(len_distinct("ccaabbb"))
