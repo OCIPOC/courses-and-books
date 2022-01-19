52d6228dd0557a80d085c118ea0c2f3622b987d6
dimastatz <dima.statz@gmail.com>
2022-01-19 21:33:34 +0200

    add Isomorphic Strings

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,16 +1,4 @@
-def len_distinct(s: str) -> int:
-    chars = {}
-    max_len, start, current = 0, 0, 0
-    while current < len(s):
-        chars[s[current]] = current
-        if len(chars) > 2:
-            min_item = min([x for x in chars.items()], key=lambda x: x[1])
-            del chars[min_item[0]]
-            start = min_item[1] + 1
-        if max_len < (current - start) + 1:
-            max_len = (current-start) + 1
-        current +=1
-    return max_len
+a = {1: 2, 2: 3}
+b = {1: 2, 2: 3}
 
-print(len_distinct("eceba"))
-# print(len_distinct("ccaabbb"))
+print(a == b)
