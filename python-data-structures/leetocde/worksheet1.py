4065081706c88df80e67ecc3b7b3ae54457a1690
dimastatz <dima.statz@gmail.com>
2022-01-21 22:56:40 +0200

    add hump game

--- python-data-structures/leetocde/worksheet1.py
+++ python-data-structures/leetocde/worksheet1.py
@@ -1,15 +1,16 @@
 def can_jump(nums: list) -> bool:
-    if len(nums) == 1:
-        return True
-    elif not nums:
-        return False
-    elif nums[0] == 0:
-        return False
-    else:
-        res = []
-        for i in range(1, nums[0] + 1):
-            res.append(can_jump(nums[i:]))
-        return True in res
+    res = set()
+    last = len(nums) - 1
+    for i in range(len(nums) - 2, -1, -1):
+        if i + nums[i] >= last:
+            res.add(i)
+        for j in res:
+            if i + nums[i] >= j:
+                res.add(i)
+                break
+    return 0 in res
+
+
 
 print(can_jump([2,3,1,1,4]))
 print(can_jump([3,2,1,0,4]))
