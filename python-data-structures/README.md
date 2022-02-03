2a1b39d854b4118e8681cf3ac7a7573367e3284e
dimastatz <dima.statz@gmail.com>
2022-02-03 16:14:23 +0200

    trap rain water

--- python-data-structures/README.md
+++ python-data-structures/README.md
@@ -70,6 +70,7 @@ In addition, in the [leetcode](https://github.com/dimastatz/courses-and-books/tr
 - [Next Permutation](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/leetcode/next-permutation.ipynb) ```#O(nlog(n))```
 - [Meeting Rooms II](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/interview-goog/meeting-rooms-2.ipynb) ```#O(nlog(n)) #priority-queue```
 - [Minimum Window Substring](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/interview-goog/min-window-substring.ipynb) ```#O(n) #sliding-window #state```
+- [Trapping Rain Water](https://github.com/dimastatz/courses-and-books/blob/master/python-data-structures/interview-goog/trap-water.ipynb) ```#O(n) #sliding-window #state```
 
 
 
--- python-data-structures/leetcode/worksheet1.py
+++ python-data-structures/leetcode/worksheet1.py
@@ -1,59 +1,12 @@
-'''
-    Example1: [a, b, c, a], [b, c]
-    - string: a state: {}
-    - string ab, state: {b:1}
-    - string abc, state: {b:1, c:1}
-    - string bc, state: {b:1, c:1}
-    - string bca, state: {b:1, c:1}
-'''
-
-def create_dict(t: str) -> dict:
-    res = {}
-    for c in t:
-        res[c] = res.get(c, (0,0))
-        res[c] = (res[c][0] + 1, 0)
+def trap(height: list) -> int:
+    res = 0
+    for i in range(1, len(height) - 1):
+        left_max = max(height[:i])
+        right_max = max(height[i+1:])
+        if right_max > height[i] and left_max > height[i]:
+            res += min(left_max, right_max) - height[i]
     return res
 
 
-def is_complete(d: dict) -> bool:
-    for x in d.values():
-        if x[0] > x[1]:
-            return False
-    return True
-
-
-def move_left(s: str, left: int, pattern: dict) -> int:
-    while left < len(s):
-        if not s[left] in pattern:
-            left += 1
-        else:
-            state = pattern[s[left]]
-            if state[1] > state[0]:
-                pattern[s[left]] = (state[0], state[1] - 1)
-                left += 1
-            else:
-                break
-    return left
-
-
-def min_window(s: str, t: str) -> str:
-    pattern = create_dict(t)
-    left, right, res = 0, 0, ''
-
-    while right < len(s):
-        if s[right] in pattern:
-            pattern[s[right]] = pattern[s[right]][0], pattern[s[right]][1] + 1 
-
-        right += 1
-        left = move_left(s, left, pattern)
-
-        if is_complete(pattern):
-            if not res or len(res) > right - left:
-                res = s[left:right]
-    return res
-
-
-assert min_window('a', 'b') == ''
-assert min_window('a', 'a') == 'a'
-assert min_window('a', 'aa') == ''
-assert min_window('ADOBECODEBANC', 'ABC') == 'BANC'
+assert trap([4,2,0,3,2,5]) == 9
+assert trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
