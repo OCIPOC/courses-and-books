c0dc07e13ba72aa7cb5371b1da651f242025f02a
dimastatz <dima.statz@gmail.com>
2022-02-17 22:30:03 +0200

    add draw

--- python-data-structures/leetcode/worksheet2.py
+++ python-data-structures/leetcode/worksheet2.py
@@ -1,1 +1,9 @@
-print(1 - pow(0.5, 4))
+import pandas as pd
+
+
+df = pd.read_parquet('/Users/dstatz/Documents/tmp/part-00003-70989667-c939-4c55-b32b-c415e4c1def9.c000.snappy.parquet')
+
+df.info()
+print(df)
+
+
