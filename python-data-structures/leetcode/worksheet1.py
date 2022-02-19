4918c7cb2354b84e4c4f30911dc61e00979fa1c2
dimastatz <dima.statz@gmail.com>
2022-02-19 22:47:41 +0200

    fb map

--- python-data-structures/leetcode/worksheet1.py
+++ python-data-structures/leetcode/worksheet1.py
@@ -1,18 +1,2 @@
-def eff_sum_array(nums: list, k) -> int:
-    dictionary = {0:1}
-    prefix_sum = 0
-    output = 0
-    
-    for num in nums:
-        print(dictionary, prefix_sum, output, num, nums)
-        prefix_sum += num
-        output += dictionary.get(prefix_sum - k,0)
-        
-        if prefix_sum not in dictionary:
-            dictionary[prefix_sum] = 1
-        else:
-            dictionary[prefix_sum] += 1
-    return output
+print('1.1.1.1'.split('.'))
 
-assert eff_sum_array([1,-1,0], 0) == 3
-        
